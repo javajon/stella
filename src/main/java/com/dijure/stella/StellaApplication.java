97dd8674a68a5b0a945a08f681b147d6c4c85e01
Jonathan <jonathan.johnson@dijure.com>
2022-04-07 17:39:18 -0400

    Upgraded to Java 17 and other dependencies

--- src/main/java/com/dijure/stella/StellaApplication.java
+++ src/main/java/com/dijure/stella/StellaApplication.java
@@ -1,6 +1,7 @@
 package com.dijure.stella;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.BufferedReader;
